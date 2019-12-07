083b38770cec254a648cbd0a207f2c1d13059c7a
Jeremiah Crowell <jcrowe6@illinois.edu>
2019-12-07 09:57:10 -0600

    Got basic server moves working!

--- app/src/main/java/com/example/checkers/MainActivity.java
+++ app/src/main/java/com/example/checkers/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.checkers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -38,8 +53,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * Should be 2/orange if the player created the game, 1/blue if they joined. */
     private int userID;
 
+    /** Server URL */
+    private String url;
+
+    /** Game password/code, determined by the creator of the game, set by the intent from the first screen. */
+    private String gamePass;
+
     /** Will toggle between 2 and 1 as play goes on */
     private int currentPlayerID;
+
+    /** Raw string response from the server*/
+    private String raw_response;
+
+    /** Use to stop the watching of the server */
+    private boolean newEntryFound;
 
     private Button recent;
     @Override
@@ -55,6 +82,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         userID = 2; /** Right here I hardcoded this as 2, but really this will be set depending on a value passed in the intent. */
         currentPlayerID = 2; // this though should always START as 2, and toggle every move
         recent = null;
+        url = "http://ec2-18-218-224-138.us-east-2.compute.amazonaws.com:8765/";
+        gamePass = "tr33"; /** Also needs to be set from intent */
+        newEntryFound = false;
 
         /**Iterates through activity_main.xml and creates a Cell object for each button.
          * This is necessary in order to pass the List to other activities (i.e GameActivity).
@@ -103,7 +133,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      *     }
      * }
      */
+    public void watchForChange(final int initialLength) {
+        System.out.println("watchForChange called!");
+        StringRequest stringRequest = new StringRequest
+                (Request.Method.GET, url + "?" + gamePass, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        raw_response = response;
+                        raw_response = raw_response.substring(2);
+                        raw_response = raw_response.substring(0, raw_response.length() - 2);
+                        String[] array = raw_response.split("', '");
+                        if (array.length != initialLength){
+                            System.out.println(array[array.length - 1]);
+                            serverMove(array[array.length - 1]);
+                            newEntryFound = true;
+                        }
+                    }
+                }, new Response.ErrorListener() {
 
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //respText.setText(error.toString());
+                        // Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+                        System.out.println(error.toString());
+                    }
+                });
+            RequestQueue queue = Volley.newRequestQueue(this);
+            queue.add(stringRequest);
+    }
+
+    /**
+     * Starts "watching" the server at the game id set above.
+     * First sends a request to the page, gets the length of the list, and creates a handler that runs every 4.5 seconds
+     * The handler, checks that newEntryFound is false, then calls watchForChange and passes the inital length.
+     * watchForChange checks the server, sees if the length is longer, and if so, sets newEntryFound to be true
+     *  and can now use the latest update to the server to call a move.
+     * This function  should not be called if newEntryFound != false to begin with
+     */
+    public void startWatching() {
+        System.out.println("startWatching called!");
+        StringRequest stringRequest = new StringRequest
+                (Request.Method.GET, url + "?" + gamePass, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        raw_response = response;
+                        raw_response = raw_response.substring(2);
+                        raw_response = raw_response.substring(0, raw_response.length() - 2);
+                        final String[] array = raw_response.split("', '");
+                        // System.out.println("sW returned, calling wFC");
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (!newEntryFound) {
+                                    watchForChange(array.length);
+                                    handler.postDelayed(this, 4500);
+                                }
+                            }
+                        }, 3000);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //respText.setText(error.toString());
+                        // Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+                        System.out.println(error.toString());
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+    }
+
+    /**
+     * Called once a new move is detected on the server, takes the concatenated data,
+     * sets the views to the necessary values for movePiece to work, and calls movePiece.
+     * @param moveData the raw, newest string on the server. 4 numbers in the form xyxy where
+     *                 the first two are the xy of the old spot and the last two are the xy of the
+     *                 new spot.
+     */
+    public void serverMove(String moveData) {
+        String oldCoords = moveData.substring(0,2);
+        String newCoords = moveData.substring(2);
+        recent = findViewById(pieceLocations.get(oldCoords));
+        View newSpot = findViewById(buttonId.get(newCoords));
+        movePiece(newSpot);
+    }
     @Override
     public void onClick(View view) {
         /**First checks if the clicked button contains a checker. If it does, that button is marked by storing it
@@ -111,6 +226,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          * button with recent's drawable. So we need to revise the code and in the if else and move it into a seperate
          * method for convenience
          */
+        startWatching();
+        System.out.println("Past startWatching!");
         if (pieceLocations.containsValue(view.getId())) { /** Here is where we're going to check current player against the user player */
             if (currentPlayerID == playerLocations.get(view.getTag().toString().substring(7))) { /** this part may be redundant once we implement the HTTP move making */
                 recent = (Button) view;
@@ -120,7 +237,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             movePiece(view);
         }
     }
-    /** Logic for moving a piece from one xy to another, including capturing other pieces.*/
+    /** Logic for moving a piece from one xy to another, including capturing other pieces.
+     * Needs recent to be set to the original Button View cell of the piece, and needs
+     * the Button View of the new spot to be passed to it*/
     public void movePiece(View view) {
         // Toast below shows the clicked button's id for debugging / me figuring this out
         // Toast.makeText(this, newCell, Toast.LENGTH_SHORT).show();
